eb5779fb75cb504275ec1a6e89a532b3d6f8998a
Frederik Dahl Hansen <hansenf@shernet.sheridancollege.ca>
2024-11-11 21:07:59 -0500

    Set up baseline UI for sign up and added logo and color assets to project

--- WeatherApp/Views/SignUpView.swift
+++ WeatherApp/Views/SignUpView.swift
@@ -8,8 +8,57 @@
 import SwiftUI
 
 struct SignUpView: View {
+    
+    @State private var userName = ""
+    @State private var password = ""
+    @State private var retypePassword = ""
+    
+    
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            VStack {
+                Text("SIGN UP").font(.title)
+                
+                Spacer()
+                
+                Image("logo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 200, height: 200)
+                
+                
+                Spacer()
+                
+                //reference: Styling inspiration and help https://stackoverflow.com/questions/67132408/i-have-trouble-using-cornerradius-and-borders-on-a-textfield-in-swiftui/67132493
+                TextField("Please enter username", text: $userName)
+                    .padding()
+                    .overlay(RoundedRectangle(cornerRadius: 10.0).strokeBorder(Color.black, style: StrokeStyle(lineWidth: 1.0)))
+                    .padding()
+                
+                //hide input reference: https://developer.apple.com/documentation/swiftui/securefield
+                TextField("Please enter password", text: $password)
+                    .padding()
+                    .overlay(RoundedRectangle(cornerRadius: 10.0).strokeBorder(Color.black, style: StrokeStyle(lineWidth: 1.0)))
+                    .padding()
+                
+                TextField("Please re-enter password", text: $retypePassword)
+                    .padding()
+                    .overlay(RoundedRectangle(cornerRadius: 10.0).strokeBorder(Color.black, style: StrokeStyle(lineWidth: 1.0)))
+                    .padding()
+                
+                Spacer()
+                
+                
+                //Placeholder
+                NavigationLink("Sign Up", destination: SignUpView())
+                    .frame(width:100 , height: 55)
+                    .background(.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(5)
+                    .font(.subheadline)
+                    .padding(4)
+            }
+        }
     }
 }
 
