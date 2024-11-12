2db18666d64d69970bafc76b044a07c70bd79176
Frederik Dahl Hansen <hansenf@shernet.sheridancollege.ca>
2024-11-11 20:25:17 -0500

    Changed routing to start app at login view. Added styling login view and routed to sign up page

--- WeatherApp/Views/LoginView.swift
+++ WeatherApp/Views/LoginView.swift
@@ -8,8 +8,57 @@
 import SwiftUI
 
 struct LoginView: View {
+    
+    @State private var userName = ""
+    @State private var password = ""
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack {
+                Text("WELCOME").font(.title)
+                
+                Spacer()
+                
+                
+                //reference: Styling inspiration and help https://stackoverflow.com/questions/67132408/i-have-trouble-using-cornerradius-and-borders-on-a-textfield-in-swiftui/67132493
+                TextField("Please enter username", text: $userName)
+                    .padding()
+                    .overlay(RoundedRectangle(cornerRadius: 10.0).strokeBorder(Color.black, style: StrokeStyle(lineWidth: 1.0)))
+                    .padding()
+                
+                //hide input reference: https://developer.apple.com/documentation/swiftui/securefield
+                TextField("Please enter password", text: $password)
+                    .padding()
+                    .overlay(RoundedRectangle(cornerRadius: 10.0).strokeBorder(Color.black, style: StrokeStyle(lineWidth: 1.0)))
+                    .padding()
+                
+                Spacer()
+                
+                HStack {
+                    Button ("Log In") {
+                        
+                    }
+                    .frame(width:100 , height: 55)
+                    .background(.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(5)
+                    .font(.subheadline)
+                    .padding(4)
+                    
+                    NavigationLink("Sign Up", destination: SignUpView())
+                        .frame(width:100 , height: 55)
+                        .background(.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(5)
+                        .font(.subheadline)
+                        .padding(4)
+                }
+                
+            }
+            
+            
+        }
     }
 }
 
