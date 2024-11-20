92f2d085d796dee5b43ccfbbcecec8224dd35a67
Frederik Dahl Hansen <hansenf@shernet.sheridancollege.ca>
2024-11-20 11:58:36 -0500

    Set up validation for the sign up and alert if input is incorrect. Created user struct. Planning on implemeting firebase to store the user details.

--- WeatherApp/Views/SignUpView.swift
+++ WeatherApp/Views/SignUpView.swift
@@ -12,7 +12,14 @@ struct SignUpView: View {
     @State private var userName = ""
     @State private var password = ""
     @State private var retypePassword = ""
+    @State private var email = ""
     
+    @State private var showAlert = false
+    
+    var isFormValid: Bool {
+            !email.isEmpty && !userName.isEmpty && !password.isEmpty && !retypePassword.isEmpty
+    }
+
     
     var body: some View {
         NavigationView {
@@ -30,18 +37,24 @@ struct SignUpView: View {
                 Spacer()
                 
                 //reference: Styling inspiration and help https://stackoverflow.com/questions/67132408/i-have-trouble-using-cornerradius-and-borders-on-a-textfield-in-swiftui/67132493
+                
+                TextField("Please enter email", text: $email)
+                    .padding()
+                    .overlay(RoundedRectangle(cornerRadius: 10.0).strokeBorder(Color.black, style: StrokeStyle(lineWidth: 1.0)))
+                    .padding()
+                
                 TextField("Please enter username", text: $userName)
                     .padding()
                     .overlay(RoundedRectangle(cornerRadius: 10.0).strokeBorder(Color.black, style: StrokeStyle(lineWidth: 1.0)))
                     .padding()
                 
                 //hide input reference: https://developer.apple.com/documentation/swiftui/securefield
-                TextField("Please enter password", text: $password)
+                SecureField("Please enter password", text: $password)
                     .padding()
                     .overlay(RoundedRectangle(cornerRadius: 10.0).strokeBorder(Color.black, style: StrokeStyle(lineWidth: 1.0)))
                     .padding()
                 
-                TextField("Please re-enter password", text: $retypePassword)
+                SecureField("Please re-enter password", text: $retypePassword)
                     .padding()
                     .overlay(RoundedRectangle(cornerRadius: 10.0).strokeBorder(Color.black, style: StrokeStyle(lineWidth: 1.0)))
                     .padding()
@@ -49,14 +62,33 @@ struct SignUpView: View {
                 Spacer()
                 
                 
-                //Placeholder
-                NavigationLink("Sign Up", destination: SignUpView())
-                    .frame(width:100 , height: 55)
-                    .background(.blue)
-                    .foregroundColor(.white)
-                    .cornerRadius(5)
-                    .font(.subheadline)
-                    .padding(4)
+                Button("Sign Up"){
+                    
+                    if (password == retypePassword){
+                        let user = User(name: userName, email: email, password: password)
+                        
+                        
+                    } else {
+                        showAlert = true
+                    }
+                    
+                }
+                .disabled(!isFormValid)
+                
+                //https://developer.apple.com/documentation/swiftui/alert
+                .alert(isPresented: $showAlert) {
+                    Alert(
+                        title: Text("Incorrect Input"),
+                        message: Text("Password inputs must match!")
+                    )
+                }
+                
+                .frame(width:100 , height: 55)
+                .background(.blue)
+                .foregroundColor(.white)
+                .cornerRadius(5)
+                .font(.subheadline)
+                .padding(4)
             }
         }
     }
