ebcc7c299514d89fb44a2e994d3687f174b392a0
Julius Nillo <nilloj@sheridancollege.ca>
2024-11-29 10:58:58 -0500

    Continuing adding to the views like Home and Favourite

---
+++ WeatherApp/Views/FavouriteView.swift
@@ -0,0 +1,18 @@
+//
+//  FavouriteView.swift
+//  WeatherApp
+//
+//  Created by Julius Nillo on 2024-11-29.
+//
+
+import SwiftUI
+
+struct FavouriteView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FavouriteView()
+}
